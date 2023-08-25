3e5a374d101816f297aea6486491567f0ff131a7
imThien <immthien407@gmail.com>
2023-08-26 01:05:25 +0700

    update code to merge

--- be_capstone_upreach/src/api/Controller/Influencer/InfluencerController.js
+++ be_capstone_upreach/src/api/Controller/Influencer/InfluencerController.js
@@ -748,7 +748,7 @@ async function addInfluencer(req, res, next) {
     const user = await userService.getUserByEmail(email);
     const now = new Date();
     const dateNow = now.toISOString();
-    if (!await addInfluencerProfile(name, nickname, email, age, phone, gender, intro, location, relationship, typeId,uploadedImages[0].url)) {
+    if (!await addInfluencerProfile(name, nickname, emailContact, age, phone, gender, intro, location, relationship, typeId,uploadedImages[0].url)) {
       return res.json({ status: 'False', message: 'Insert Data Influencer Profiles Fails' });
     }
     if (!await addInfluencerKols(user.userId, 0, null)) {
